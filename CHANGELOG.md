3ef049cf602164e5dd3d238691b6bfabdf27a4d2
DwarfMaster <luc@dwarfmaster.net>
2019-09-01 12:27:49 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for rsscli
+
+## 0.0.1 -- 2019-08-31
+
+* First version. Released on an unsuspecting world.
---
+++ default.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, base, download, feed, relude, sqlite-simple, stdenv
+}:
+mkDerivation {
+  pname = "rsscli";
+  version = "0.0.1";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base download feed relude sqlite-simple
+  ];
+  homepage = "https://github.com/lucas8/rsscli";
+  description = "A newsboat compatible rss cli";
+  license = stdenv.lib.licenses.mit;
+
+  shellHook = ''
+    export HIE_HOOGLE_DATABASE="$(readlink -f $(whereis ghc | cut -d' ' -f 2) | xargs dirname)/../share/doc/hoogle/index.html"
+  '';
+}
